f7c87806c516d619386cab2f00d19a14642a3b3f
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2024-02-13 14:28:40 +0530

    feat: added sidebar and next and previous function metthods

--- job-portal-client/src/Pages/Home.jsx
+++ job-portal-client/src/Pages/Home.jsx
@@ -7,13 +7,18 @@ import Sidebar from "../sidebar/Sidebar";
 const Home = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
 
   useEffect(() => {
+    setIsLoading(true);
     fetch("jobs.json")
       .then((res) => res.json())
       .then((data) => {
         // console.log(data);
         setJobs(data);
+        setIsLoading(false);
       });
   }, []);
 
@@ -40,6 +45,28 @@ const Home = () => {
   const handleClick = (event) => {
     setSelectedCategory(event.target.value);
   };
+
+  // calculate the index range
+  const calculatePageRange = () =>{
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return {startIndex, endIndex}
+  }
+
+  // function for the next page
+  const nextPage = () =>{
+    if(currentPage < Math.ceil(filteredItems.length / itemsPerPage)){
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  // function for the previous page
+  const prevPage = () =>{
+    if(currentPage >1){
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
 
   // ..... main function.....
   const filteredData = (jobs, selected, query) => {
@@ -80,18 +107,24 @@ const Home = () => {
 
         {/* main content */}
         <div className="bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12">
-
-           {/* Left Side */}
+          {/* Left Side */}
           <div className="bg-white p-4 rounded">
-            <Sidebar handleChange={handleChange} handleClick={handleClick}/>
+            <Sidebar handleChange={handleChange} handleClick={handleClick} />
           </div>
 
-           {/*Job Card */}
-          <div className="col-span-2 bg-white p-4 rounded-sm"><Jobs result={result} /></div>
+          {/*Job Card */}
+          <div className="col-span-2 bg-white p-4 rounded-sm">
+            {
+              isLoading ? (<p className="font-medium">Loading...</p>) : result.length > 0 ? (<Jobs result={result} />) : <>
+              <h3 className="text-lg font-bold mb-2">{result.length} Jobs</h3>
+              <p>No data found...!</p>
+              </>
+            }
+            
+          </div>
 
-           {/* Right Side */}
+          {/* Right Side */}
           <div className="bg-white p-4 rounded">Right</div>
-          
         </div>
       </div>
     </>
